6bcffd5526ce052c80d27a1f03e08595f47e76f2
preeti <pritich0896@gmail.com>
2023-01-03 18:03:14 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AddressBookSystem
+{
+    public class ContactsArrayList
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+        }
+ 
+    }
+}
