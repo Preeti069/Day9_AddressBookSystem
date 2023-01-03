3452a036052c829b0b513ba9e5512920e052a2f0
preeti <pritich0896@gmail.com>
2023-01-03 22:13:47 +0530

    create a Contacts in Address Book

--- Program.cs
+++ Program.cs
@@ -1,11 +1,45 @@
 ﻿namespace AddressBookSystem
 {
-    public class ContactsArrayList
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("Address Book System");
+            CreateContact CreateContact = new CreateContact();
+
+            Console.WriteLine("Enter the first name :");
+            CreateContact.First_Name = Console.ReadLine();
+
+            Console.WriteLine("Enter the last name :");
+            CreateContact.Last_Name = Console.ReadLine();
+
+            Console.WriteLine("Enter the Address :");
+            CreateContact.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter the city name :");
+            CreateContact.City = Console.ReadLine();
+
+            Console.WriteLine("Enter the state name :");
+            CreateContact.State = Console.ReadLine();
+
+            Console.WriteLine("Enter the zip number :");
+            CreateContact.Zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the phone number :");
+            CreateContact.Phone_Number = (int)Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter the E mail id :");
+            CreateContact.E_mail_Id = Console.ReadLine();
+
+            Console.WriteLine(CreateContact.First_Name);
+            Console.WriteLine(CreateContact.Last_Name);
+            Console.WriteLine(CreateContact.Address);
+            Console.WriteLine(CreateContact.City);
+            Console.WriteLine(CreateContact.State);
+            Console.WriteLine(CreateContact.Zip);
+            Console.WriteLine(CreateContact.Phone_Number);
+            Console.WriteLine(CreateContact.E_mail_Id);
+
         }
- 
     }
 }
